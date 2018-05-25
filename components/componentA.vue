01cac4eb331b51e8937132ee28163a094241c5c1
double518 <37170846+double518@users.noreply.github.com>
2018-05-25 20:15:49 +0800

    Add files via upload

---
+++ components/componentA.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button v-on:click="onClickMe">open mouse!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'hello from component a'
+    }
+  },
+  props:['msgfromfather'],
+  methods: {
+    onClickMe: function(){
+      //console.log(this.msgfromfather)
+      this.$emit('child-tell-me-sth', this.msg)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+</style>
